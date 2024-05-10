178c2e6dfc8afbb642ed41bc3f29be13435d03f2
unknown <abhayagnihotri1585@gmail.com>
2024-05-10 13:49:18 +0530

    userCanAccesssApp-after-signup

--- routes/user.js
+++ routes/user.js
@@ -1,42 +1,48 @@
-const {Router} = require('express')
-const User = require('../models/user');
+const { Router } = require("express");
+const User = require("../models/user");
+const { genrateTokenForUser } = require("../services/auth");
 
 const router = Router();
 
-router.get('/signin',(req,res)=>{
-    res.render('signin');
-})
-router.get('/signup',(req,res)=>{
-    res.render('signup');
-})
+router.get("/signin", (req, res) => {
+  res.render("signin");
+});
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
-router.post('/signin',async(req,res)=>{
-    const {email,password} = req.body;
-    try {
-        const token = await User.matchPasswordAndGenerateToken(email,password);
-        if(!token) return res.redirect('/signin')
-        return res.cookie('token',token).redirect('/');
-
-        } catch (error) {
-           return res.render('signin',{
-            error : "Incorrect email or Password"
-        });
-        
-    }
-})
-router.post('/signup',async(req,res)=>{
-    console.log(req.body)
-    const {fullName,email,password} = req.body;
-    await User.create({
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const token = await User.matchPasswordAndGenerateToken(email, password);
+    if (!token) return res.redirect("/signin");
+    return res.cookie("token", token).redirect("/");
+  } catch (error) {
+    return res.render("signin", {
+      error: "Incorrect email or Password",
+    });
+  }
+});
+router.post("/signup", async (req, res) => {
+  // console.log(req.body)
+  try {
+    const { fullName, email, password } = req.body;
+    const user = await User.create({
       fullName,
       email,
       password,
-    })
-    return res.redirect('/');
-})
+    });
+    const token = genrateTokenForUser(user);
+    if (!token) res.redirect("/signup");
+    return res.cookie("token", token).redirect("/");
+  } catch (error) {
+    console.log(error.message);
+    return redirect("/signup");
+  }
+});
 
-router.get('/logout',(req,res)=>{
-    res.clearCookie('token').redirect('/');
-})
+router.get("/logout", (req, res) => {
+  res.clearCookie("token").redirect("/");
+});
 
 module.exports = router;
